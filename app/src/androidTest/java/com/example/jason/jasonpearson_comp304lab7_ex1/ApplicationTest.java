80336eb1b250138ccf1a3e1bb135a1f262576639
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2016-11-26 18:13:12 -0500

    Initial Commit - To Add Repo to Remote

---
+++ app/src/androidTest/java/com/example/jason/jasonpearson_comp304lab7_ex1/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.jason.jasonpearson_comp304lab7_ex1;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/example/jason/jasonpearson_comp304lab7_ex1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jason.jasonpearson_comp304lab7_ex1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

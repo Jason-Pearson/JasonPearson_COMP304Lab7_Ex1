c8771711a22dda4d94c663644e2cb3f03e267fd0
Jason-Pearson <jason-pearson-1-0-7@hotmail.com>
2016-11-26 20:33:07 -0500

    Not sure of Textview Object, onResume, and Receiver method Everything is workable to this point, until I start fiddling around with the code for the Service message to appear to in Textview

--- app/src/main/java/com/example/jason/jasonpearson_comp304lab7_ex1/MySimpleService.java
+++ app/src/main/java/com/example/jason/jasonpearson_comp304lab7_ex1/MySimpleService.java
@@ -6,6 +6,9 @@ import android.widget.Toast;
 
 public class MySimpleService extends Service{
 
+    public static final String INFO_INTENT =
+            "com.example.jason.jasonpearson_comp304lab7_ex1.Services.INFO_UPDATE"; //replace with your package name
+
     @Override
     public IBinder onBind(Intent arg0) {
         return null;
@@ -15,6 +18,13 @@ public class MySimpleService extends Service{
     public int onStartCommand(Intent intent, int flags, int startId) {
         // We want this service to continue running until it is explicitly
         // stopped, so return sticky.
+
+        //Add Intent Object Reference - for String Extras
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction(INFO_INTENT);
+        broadcastIntent.putExtra(INFO_INTENT, "Hello There");
+        this.sendBroadcast(broadcastIntent);
+
         Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
 
         return START_STICKY;
--- app/src/main/java/com/example/jason/jasonpearson_comp304lab7_ex1/ServicesActivity.java
+++ app/src/main/java/com/example/jason/jasonpearson_comp304lab7_ex1/ServicesActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.content.ComponentName;
 import android.os.IBinder;
@@ -17,22 +18,48 @@ import android.content.ServiceConnection;
 
 
 public class ServicesActivity extends Activity {
+    //TextView textView = (TextView) findViewById(R.id.textview);
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        //TextView textView = (TextView) findViewById(R.id.textview); //-put TextView in onCreate method?
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_services);
-//
     }
+    //Do I call this method at all?...yes?...nah?
+    public void onResume()
+    {
+        super.onResume();
+        //This needs to be in the activity that will end up receiving the broadcast
+        registerReceiver(receiver, new IntentFilter("com.example.jason.jasonpearson_comp304lab7_ex1.Services.INFO_UPDATE"));
 
+    }
     public void startService(View view) {
         startService(new Intent(getBaseContext(), MySimpleService.class));
+        //onResume();
     }
 
     public void stopService(View view) {
         stopService(new Intent(getBaseContext(),
                 MySimpleService.class));
     }
+
+
+    //This will handle the broadcast
+    public BroadcastReceiver receiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //textView.setText("Here");
+            String action = intent.getAction();
+            if (action.equals(MySimpleService.INFO_INTENT)) {
+                String info = intent.getStringExtra("com.example.jason.jasonpearson_comp304lab7_ex1.Services.INFO_UPDATE");
+                //textView.setText(info);
+            }
+        }
+
+    };
 
 }
